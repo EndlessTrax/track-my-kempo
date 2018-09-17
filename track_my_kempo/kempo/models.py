dbc669e96b3e6d5f3c4e7f8c2a9626af3cc09457
RWhiteDev <contact@rickywhite.net>
2018-09-17 06:49:25 -0400

    Technique Model created and added to Admin CMS

--- track_my_kempo/kempo/models.py
+++ track_my_kempo/kempo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Technique(models.Model):
+    title = models.CharField(max_length=50)
+    # Consider changing default DateTime
+    date_practiced = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} was practiced last on: {self.date_practiced}'
