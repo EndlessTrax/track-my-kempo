d7aff1fe467b4c1601c442d41697e53b06143aa6
RWhiteDev <contact@rickywhite.net>
2018-09-18 15:57:32 -0400

    Added registration form page and logic

--- track_my_kempo/kempo/urls.py
+++ track_my_kempo/kempo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('training/', views.training_log, name='training-log')
+    path('training/', views.training_log, name='training-log'),
+    path('register/', views.register, name='register'),
 ]
