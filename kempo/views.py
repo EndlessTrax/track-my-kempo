a4aeb4b402a5ee285e98644f7ab113bc3f1a836f
RWhiteDev <contact@rickywhite.net>
2018-09-24 22:05:27 -0400

    Hopefully fixed Timezone bug

--- kempo/views.py
+++ kempo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+# from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import UserRegisterForm, AddTechnique
